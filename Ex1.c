71b19023816a39b855e5314f80dbc80beb08667b
Nildo <adejanildosp@gmail.com>
2021-09-15 16:32:04 -0300

    Create Ex1.c

---
+++ Ex1.c
@@ -0,0 +1,19 @@
+// Desenvolvimento de calculadora para realizar cálculos de subtração
+#include <stdio.h>
+#include<locale.h>
+int main()
+{
+Setlocale (LC_ALL, "");
+// Declaração de Variáveis
+float numero1, numero2, resultado; 
+// Entrada de dados
+printf("Primeiro número");
+scanf("%f", &numero1);//O %f é pra definir o tipo de dado, neste caso ‘float’- & comercial é para localizar a variável  
+printf("Segundo número");
+scanf("%f", &numero2);
+// processamento de dados
+resultado = numero1 - numero2; // O ‘=’ é um sinal de atribuição
+// saída de dados
+printf("\n Resultado da subtração: %.2f ",resultado); // \n é utilizado pra mudar de linha e o 2 no ‘%2f’ é para definir o número de casas decimais 
+return 0;
+}
