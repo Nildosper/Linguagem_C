fada1c8317be235b64f688bf2147572c0515007e
Nildo <adejanildosp@gmail.com>
2021-10-05 16:26:07 -0300

    Trabalhando com estrutura condicional encadeada

--- problemas_resolvidos/Ex11.C
+++ problemas_resolvidos/Ex11.C
@@ -11,9 +11,26 @@ int main()
 // Define codificação como sendo UTF-8
   SetConsoleOutputCP(CPAGE_UTF8);
 // declaração de variáveis
-
-
-
+float custoFabrica, alicotaDistribuidor, alicotaImposto;
+//Entrada de dados
+printf("-------------------------------------------------------------------------------");
+printf("\nInforme o preço de fábrica..: R$ ");
+scanf("%f", &custoFabrica);
+//Processamento
+printf("-------------------------------------------------------------------------------");
+if (custoFabrica > 25000)
+{
+    alicotaDistribuidor = 0.15; alicotaImposto = 0.20;
+}
+        else if (custoFabrica > 12000)
+        {
+            alicotaDistribuidor = 0.10; alicotaImposto = 0.15;
+        }
+            else
+            {
+                alicotaDistribuidor = 0.05; alicotaImposto = 0.0;
+            }
+printf("\nO preço ao consumidor será R$ %.2f \n",((custoFabrica)+(custoFabrica*alicotaDistribuidor)+(custoFabrica*alicotaImposto)));
 system("pause");
 return 0;
 // Retorna codificação padrão do Windows
