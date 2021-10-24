25fb884b67a3fc03de97995f72de26656e855cee
Nildo <adejanildosp@gmail.com>
2021-10-24 13:41:33 -0300

    Em construçao

--- E-commerce_em_C/E-commerce.C
+++ E-commerce_em_C/E-commerce.C
@@ -35,8 +35,47 @@ printf("0 - Sair do site\n");
 printf("Opção: ");
 scanf("%d", &op);
 
+switch(op){
+        case 1:
+            printf("---------------------------------------------------------------------------------------------------------\n\n");
+            printf("|                                                BRPETS                                                 |\n\n");
+            printf("---------------------------------------------------------------------------------------------------------\n\n");
+            printf("\n| A BRPet é uma empresa que surgiu para fazer a relação entre você e seu pet ser melhor a cada dia!     |\n");
+	     printf("\n| Somos apaixonados por pets e , por isso, a nossa loja foi preparada e organizada para você encontrar  |\n"); 
+            printf("\n| facilmente o que procura a qualquer hora e em qualquer lugar.                                         |\n");
+	     printf("\n| Os nossos principais produtos são as PetBOX, uma maneira inovadora de ter todo mês na sua casa tudo   |\n");
+            printf("\n| que seu pet precisa.                                                                                  |\n");   
+	     printf("\n| Faça parte do nosso Clube Pet  e aproveite o que há de melhor no mundo pet!                           |\n");
+            printf("---------------------------------------------------------------------------------------------------------\n\n");
+            system("pause");
+            system("cls");
+            main();
 
-
+            break;
+        case 2:
+            printf("Em contrução \n");
+            break;
+        case 3:
+           printf("Em contrução \n");
+            break;
+        case 4:
+            printf("Em contrução \n");
+            break;
+        case 5:
+           printf("Em contrução \n");
+            break;
+        case 6:
+            printf("Em contrução \n");
+            break;
+        case 0:
+            exit(0);
+        default:
+            printf("Digite uma opção valida: \n");
+            system("pause");
+            system("cls");
+            main();
+    }
+ 
 
 return 0;
 // Retorna codificação padrão do Windows
